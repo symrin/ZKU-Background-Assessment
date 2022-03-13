30067c1b261f8e2747a31c9ddf348fe7d7ca3536
Simran Kaur <kaursimran98@gmail.com>
2022-03-13 21:54:46 +0530

    Create App.jsx

---
+++ Week - 1 | Q2/Application/App.jsx
@@ -0,0 +1,147 @@
+import React, { useEffect, useState } from "react";
+import './styles/App.css';
+import { ethers } from "ethers";
+import myNft from '/utils/MyNFT.json';
+
+const OPENSEA_LINK = '';
+const TOTAL_MINT_COUNT = 50;
+
+const CONTRACT_ADDRESS = "0xf5374ff38576feb9a51244fb18591acbdfcaa1fb";
+
+const App = () => {
+
+    const [currentAccount, setCurrentAccount] = useState("");
+    
+    const checkIfWalletIsConnected = async () => {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+          console.log("Make sure you have metamask!");
+          return;
+      } else {
+          console.log("We have the ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+      if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log("Found an authorized account:", account);
+					setCurrentAccount(account)
+          
+          // Setup listener for where a user comes to the site and already had their wallet connected.
+          setupEventListener()
+      } else {
+          console.log("No authorized account found")
+      }
+  }
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+
+      // Setup listener for the case where a user comes to the site and connected their wallet for the first time.
+      setupEventListener() 
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // Setup listener.
+  const setupEventListener = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myNft, signer);
+
+        // This will capture the event when MintNFT contract throws it.
+        connectedContract.on("NewMintedNFT", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          alert(`NFT MINTED AT: Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}
+          we've minted ${tokenId.toNumber()}/50 NFTs
+          `)
+        });
+
+        
+
+        console.log("Setup event listener!")
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const askContractToMintNft = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myNft, signer);
+
+        console.log("Going to pop wallet now to pay gas...")
+        let nftTxn = await connectedContract.getNFTsMintedSoFar();
+
+        console.log("Mining...please wait.")
+        await nftTxn.wait();
+        console.log(nftTxn);
+        console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
+  const renderNotConnectedContainer = () => (
+    <button onClick={connectWallet} className="cta-button connect-wallet-button">
+      Connect to Wallet
+    </button>
+  );
+
+  const renderMintUI = () => (
+    <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
+      Mint NFT
+    </button>
+  )
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header gradient-text">My NFT Collection</p>
+          <p className="sub-text">
+            Each unique. Each beautiful. Discover your NFT today.
+          </p>
+          {currentAccount === "" ? renderNotConnectedContainer() : renderMintUI()}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
